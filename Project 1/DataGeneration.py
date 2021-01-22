f142e5a5f62d1cbe4d9cf0c7eacdcd4bc61454d2
martahal <martahal@stud.ntnu.no>
2021-01-22 11:49:08 +0100

    generate dataset in compliance with task, except for user specified centering of figures

--- Project 1/DataGeneration.py
+++ Project 1/DataGeneration.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class DataGeneration:
-    def __init__(self, noiseParam=0, imgSize=10, setSize=100, flatten=False, trainValTest=(0.7, 0.2, 0.1)):
+    def __init__(self, noiseParam=0.0, imgSize=10, setSize=100, flatten=False, trainValTest=(0.7, 0.2, 0.1)):
         self.noiseParam = noiseParam
         self.imgSize = imgSize
         self.setSize = setSize
@@ -20,20 +20,20 @@ class DataGeneration:
         for i in range(self.setSize):
             # the full set is portioned with roughly 1/4 of each image category
             if i > self.setSize * 0.75:
-                full_set.append(self.gen_image(self.imgSize, 'blob', self.flatten))
+                full_set.append(self.gen_image(self.imgSize, 'blob', self.noiseParam))
             elif i > self.setSize * 0.5:
-                full_set.append(self.gen_image(self.imgSize, 'bars', self.flatten))
+                full_set.append(self.gen_image(self.imgSize, 'bars', self.noiseParam))
             elif i > self.setSize * 0.25:
-                full_set.append(self.gen_image(self.imgSize, 'rect', self.flatten))
+                full_set.append(self.gen_image(self.imgSize, 'rect', self.noiseParam))
             else:
-                full_set.append(self.gen_image(self.imgSize, 'cross', self.flatten))
+                full_set.append(self.gen_image(self.imgSize, 'cross', self.noiseParam))
         np.random.shuffle(full_set)
 
         if (sum(self.trainValTest)- 0.01)**2 < 1 or (sum(self.trainValTest)- 0.01)**2  == 1:
             # Dividing the shuffled full set into training set, validation set and test set
-            train_proportion = int(round(self.trainValTest[0] * len(full_set)))
-            val_proportion = int(round(self.trainValTest[1] * len(full_set)))
-            test_proportion = int(round(self.trainValTest[2] * len(full_set)))
+            train_proportion = round(self.trainValTest[0] * len(full_set))
+            val_proportion = round(self.trainValTest[1] * len(full_set))
+            test_proportion = round(self.trainValTest[2] * len(full_set))
             self.trainSet = full_set[:train_proportion]
             self.valSet = full_set[:train_proportion + val_proportion]
             self.testSet = full_set[:train_proportion + val_proportion + test_proportion]
@@ -45,14 +45,15 @@ class DataGeneration:
         for image in draw_selection:
             self.draw_image(image)
 
-    @staticmethod
-    def gen_image(self, n, shape, flatten):
+     #@staticmethod
+    def gen_image(self, n, shape, noise):
         """Create one image with one of the four desired shapes.
-        Positions and sizes of shapes are generated randomly for each image.
-        Image is flattened if flattened = True
-        Returns a tuple: (label, image)"""
-        # TODO Implement noise
+        Positions and sizes of shapes are generated randomly for each image.'
+        Noise is implemented by setting a user-defined portion of array entries to the opposite value
+        at the end of the image generation.
+        Returns a tuple: (label, image, flattened image)"""
         image = np.zeros((n, n))
+
         if shape == 'cross':
             # length of cross arms are random odd length
             l = (np.random.randint(2,np.floor(n/2))*2) - 1
@@ -60,9 +61,8 @@ class DataGeneration:
             image[start_pos[0] + int(np.floor(l/2))][start_pos[1] : start_pos[1] + l] = 1
             for i in range(start_pos[0], start_pos[0] + l):
                 image[i][start_pos[1] + int(np.floor(l/2))] = 1
-            if flatten:
-                flat_image = image.flatten()
-                image = flat_image
+
+
         if shape == 'rect':
             l = np.random.randint(2,n)
             w = np.random.randint(2,n)
@@ -72,9 +72,8 @@ class DataGeneration:
             for i in range(start_pos[0], start_pos[0] + l + 1):
                 image[i][start_pos[1]]= 1  # left side
                 image[i][start_pos[1] + w] = 1 # right side
-            if flatten:
-                flat_image = image.flatten()
-                image = flat_image
+            flat_image = image.flatten()
+
         if shape == 'bars':
             # Draw bars at random columns
             cols = np.zeros(n)
@@ -83,9 +82,7 @@ class DataGeneration:
             for i in range(n):
                 if cols[i] == 1:
                     image[i] = 1
-            if flatten:
-                flat_image = image.flatten()
-                image = flat_image
+
         elif shape == 'blob':
             r = np.random.randint(3, n/2)  # To avoid same shape as cross, min r = 2
             center = [np.random.randint(r, n - r), np.random.randint(r, n - r)]
@@ -93,21 +90,33 @@ class DataGeneration:
                 for j in range(n):
                     if np.ceil(np.sqrt((center[0] - i)**2 + (center[1] - j)**2)) < r :
                         image[i][j] = 1
-            if flatten:
-                flat_image = image.flatten()
-                image = flat_image
-        return shape, image
+
+
+        if noise > 1 or noise < 0:
+            print("Noise parameter must be between 0 and 1")
+        else:
+            #Select a portion of the array to flip
+            flipped_pixels = round((len(image) * n) * noise)
+            for i in range(flipped_pixels):
+                rdm_pixel = (np.random.randint(0, len(image)), np.random.randint(0, n))
+                if image[rdm_pixel[0], rdm_pixel[1]] == 0:
+                    image[rdm_pixel[0], rdm_pixel[1]] = 1
+                else:
+                    image[rdm_pixel[0], rdm_pixel[1]] = 0
+
+        flat_image = image.flatten()
+
+        return shape, image, flat_image
 
     @staticmethod
     def draw_image(data):
-        # TODO How to draw if array is flattened?
         plt.title(data[0])
         plt.imshow(data[1], cmap='gray')
         plt.show()
 
 
 if __name__ == '__main__':
-    data1 = DataGeneration(imgSize=10, setSize= 100)
+    data1 = DataGeneration(noiseParam=0.0, imgSize=50, setSize= 100)
     data1.gen_dataset()
     #print(gen_array(10,'cross'))
     #draw_image(gen_array(10,'blob'))
