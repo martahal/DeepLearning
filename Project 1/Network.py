64ce3a75eae619cb09f655f13b9ef4125c7f845a
martahal <martahal@stud.ntnu.no>
2021-01-25 14:46:19 +0100

    Created initial skeleton for Network class

---
+++ Project 1/Network.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class Network:
+
+    def __init__(self, cost_function='MSE'):
+        self.cost_function = cost_function
+
+    def gen_layers(self):
+        pass
+
+    def train(self):
+        pass
+
+    def _forward_pass(self):
+        pass
+
+    def _backward_pass(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
