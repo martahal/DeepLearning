Correct backprop, and config by file. it learns

Project1/Layer.py
@@ -4,11 +4,11 @@ from Projects.Project1 import DataGeneration
 
 class Layer:
 
-    def __init__(self, size, activation_func, l_type,  w_range=(-1.0, 1.0), lr=0.01):
-        self.size = size
+    def __init__(self, size, activation_func, l_type, optionals, global_lr):
+        self.size = int(size)
         self.activation_func = activation_func
-        self.w_range = w_range
-        self.lr = lr
+        self.w_range = optionals['w_range'] if 'w_range' in optionals else (-0.1, 0.1)
+        self.lr = optionals['lr'] if 'lr' in optionals else global_lr
         self.l_type = l_type
         self.weights = None
         self.bias_vector = None
@@ -16,6 +16,7 @@ class Layer:
         self.bias_gradient = None
         self.bias_node = 1
         self.cached_activation = []  # caching activation vector to simplify calculation of derivative when backpropping.
+
 
     def activation(self, z):
         """Computes the activation of the input vector z with the activation function defined for this particular layer.
@@ -56,36 +57,22 @@ class Layer:
         if self.activation_func == 'softmax':  # Softmax layer does not take any weights
             W = None
         else:
-            W = np.zeros(shape=(prev_layer_size, self.size))
-            #W = np.random.uniform(self.w_range[0], self.w_range[1], (prev_layer_size, self.size))
+            #W = np.ones(shape=(prev_layer_size, self.size))
+            W = np.random.uniform(self.w_range[0], self.w_range[1], (prev_layer_size, self.size))
         self.weights = W
 
     def gen_bias(self):
         """Initializes a bias vector with all values equal to 0"""
         self.bias_vector = np.zeros(self.size)
 
-    def forward_pass(self, image_data):
+    def forward_pass(self, inputs):
         if self.l_type == 'input' or self.activation_func == 'softmax':
-            x = self.activation(image_data)
+            x = self.activation(inputs)
         else:
-            z = np.add(np.dot(image_data, self.weights), np.dot(self.bias_vector, self.bias_node))
+            z = np.add(np.einsum('ij,jk->ik',inputs, self.weights), np.dot(self.bias_vector, self.bias_node))
             x = self.activation(z)
         self.cached_activation = x
         return x
-
-    def backward_pass(self, deltas, upstream_activations):  #TODO YOU ARE HERE: problem: self.derivation gives an array of derived activation for all activations from the minibatch
-        """Lots of nasty tensor calculus happens here"""
-        new_deltas = []
-        weight_gradients = []
-        j_n_sum = self.derivation()
-        dn_dm = np.dot(j_n_sum, self.weights)
-
-        # for i in range(len(deltas)):
-        #     new_deltas.append(np.dot(deltas[i], dn_dm[i]))  #TODO check if this is mathematically correct
-        #
-        # new_deltas = np.array(new_deltas)
-
-        return new_deltas
 
     def update_weights_and_bias(self):
         """Sums and averages the array of weight and bias gradients into one weight gradient and one bias gradient
@@ -110,9 +97,10 @@ class Layer:
         return np.array([1 - x_i**2 for x_i in x])
 
     def _relu(self, z):
-        activation = []
-        for z_i in z:
-            activation.append(max(0, z_i)) # TODO Something brakes here
+        activation = np.copy(z)
+        for i in range(len(z)):
+            for j in range(len(z[i])):
+                activation[i][j] = (max(0, z[i][j]))
         return np.array(activation)
 
     def _d_relu(self, x):
@@ -128,9 +116,15 @@ class Layer:
     def _d_linear(self, x):
         return np.ones_like(x)
 
-    def _softmax(self, z):
-        sum_z = np.sum(np.exp([z_i for z_i in z]))
-        softmaxed = np.array([np.exp(z_i)/sum_z for z_i in z])
+    def _softmax(self, z_array):
+        # Writing out all steps to not get this calculation wrong
+        z_sums = []
+        for z in z_array:
+            z_sums.append(np.sum(np.exp([z_i for z_i in z])))
+        softmaxed = []
+        for i in range(len(z_sums)):
+            softmaxed.append(np.exp(z_array[i])/z_sums[i])
+        softmaxed = np.array(softmaxed)
         return softmaxed
 
     def _d_softmax(self, activations):
@@ -151,12 +145,15 @@ class Layer:
 
 
 def main():
-    l1 = Layer(size=2, activation_func='linear')
-    z = np.random.random(3)
-    x = l1.activation(z)
-    print(x)
-    something = l1.derivation()
-    print(something)
+    l1 = Layer(size=4, activation_func='softmax', l_type='softmax', optionals={}, global_lr=0.1)
+    z = np.array([[1,1,2,1,1],
+         [1,1,2,-1,-1]])
+    # test_softmax = Layer._softmax(z)
+
+    test_softmax = l1.activation(z)
+    print(test_softmax)
+    test_d_softmax = l1.derivation()
+    print(test_d_softmax)
 
 
 if __name__ == '__main__':