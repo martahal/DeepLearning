1c87ab42b04c333a4f17832b13c889eff67a564c
martahal <martahal@stud.ntnu.no>
2021-04-06 10:15:06 +0200

    Started implementing autoencoder. Swithcing branch

--- Project3/Autoencoder.py
+++ Project3/Autoencoder.py
@@ -8,15 +8,17 @@ class Autoencoder(nn.Module):
 
     def __init__(self,
                  encoder,
-                 decoder):
+                 decoder,
+                 num_classes):
         super().__init__()
 
         self.encoder = encoder
         self.decoder = decoder
 
+        self.num_classes = num_classes
     def forward(self, x):
         """
         Performs the forward pass of the autoencoder
         :param x: Input image, shape: [batch_size, image channels, width, height]
-        :return:
+        :return: reconstructed images, shape [batch_size, image channels, width, height]
         """
